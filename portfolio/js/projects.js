88ae0eff6c84b2fc938e6d65ed3815da25727351
Gloria Guy <totallygloria@gmail.com>
2016-09-22 15:58:58 -0700

    Moved my html templating into a Handlebars script tag at the top of the page, changed the project prototype to select the html and then dynamically populate it using Handlebars to replace values from my projects data-type. The update html is appended to the page using the exact same functionality as the previous html templating.
    Page displays correctly, but the category drop-down no longer works.

--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -1,37 +1,27 @@
 // This app reads project data from the projectList.js file and populates the
-// index.html page using jQuery.
+// index.html page using Handlebars.
 
 var projectArray = [];
 
 function Projects (data) {
-    this.title = data.title;
-    this.link = data.link;
-    this.category = data.category;
-    this.body = data.body;
-    this.imagesrc = data.imagesrc;
-    this.imagealt = data.imagealt;
-    this.pubDate = data.pubDate;
+    for (var key in data)
+        this[key] = data[key];
 }
 
 Projects.prototype.createHtml = function() {
     // This function grabs the template section of the page as a jQuery object,
-    // populates its values with the details from a project, then returns an
-    // updated object, without the template class
+    // and uses Handlebars to populates its values with the details from a
+    // the projectList array
 
-    var $currentProject = $('article.projectsTemplate').clone();
-    $currentProject.find('h2').text(this.title);
-    $currentProject.find('a').not('.read-more').attr('href', this.link);
-    $currentProject.filter('article').attr('data-category', this.category);
-    $currentProject.find('p.projectBody').html(this.body);
-    $currentProject.find('img').attr('src', this.imagesrc);
-    $currentProject.find('img').attr('alt', this.imagealt);
-    $currentProject.find('div').text(this.pubDate + ',');
-
-    $currentProject.find('time[pubdate]').attr('title', this.pubDate);
-    $currentProject.find('time').html('about ' + parseInt(new Date() - new Date(this.pubDate))/3600/24/1000 + ' days ago.');
-
-    $currentProject.removeClass('projectsTemplate');
-    return $currentProject;
+    var $sourceHtml = $('#projects-template').html();
+    var template = Handlebars.compile($sourceHtml);
+    this.pubDate = this.pubDate ?
+        'Published on: ' + this.pubDate + ", about " +
+        parseInt(new Date() - new Date(this.pubDate)/3600/24/1000) +
+        'days ago.' : '(draft)';
+    var newHtml = template(this);
+    
+    return newHtml;
 };
 
 myBlogPosts.sort(function(cur, next) {
