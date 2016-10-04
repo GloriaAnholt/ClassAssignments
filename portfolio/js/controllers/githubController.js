0057b1b0fa0bae11b37520a78889211acf806c04
Gloria Guy <totallygloria@gmail.com>
2016-10-04 16:18:37 -0700

    Changed the menu bar entry for Github from a link to a tab, started the basics of a tabbed section. Added internal route to hit tab page. Created a controller (still empty) for desired behavior on a new GH tab click.

---
+++ portfolio/js/controllers/githubController.js
@@ -0,0 +1,11 @@
+(function(module){
+    // Compiles the handlebars template and then populates it with data from
+    // the Github API
+
+    var githubView = {};
+
+
+
+
+    module.githubView = githubView;
+})(window);
