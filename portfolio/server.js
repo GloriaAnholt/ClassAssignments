76db1f34f9f6a4034ab37a0876305e6cc726131f
Gloria Guy <totallygloria@gmail.com>
2016-10-05 14:56:19 -0700

    Modified the server file to now capture internal requests for github and to route them through my own web server in order to keep the secret token secret. The url address is appended to the base api url.
    Also added express-request-proxy package to my package.json, in
    order to use this express library.

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,8 +1,22 @@
 // Let's build a server!
 var express = require('express'),
+  proxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
+
+app.get('/github/*', function(request, response){
+  console.log('External Github request made: ', request.url);
+  // If a git hub request happens internally, glue it together here:
+  (proxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.GITHUB_TOKEN
+    }
+  }))(request, response);
+});
+
+// The serve-all has to be below the specific requests or it overrides them
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
