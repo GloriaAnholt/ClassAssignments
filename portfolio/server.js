9b58089e57bea3033d69128f34402eabfcd3e739
Gloria Guy <totallygloria@gmail.com>
2016-10-05 15:08:13 -0700

    Got a twitter authorization token and added the web server routing for handling twitter requests using a token. (Note: the page doesn't currently hit the twitter api, but I'd like to display the last few tweets).

--- portfolio/server.js
+++ portfolio/server.js
@@ -16,6 +16,17 @@ app.get('/github/*', function(request, response){
   }))(request, response);
 });
 
+app.get('/twitter/*', function(request, response){
+  console.log('External Twitter request made: ', request.url);
+  // If a twitter request happens internally, glue it together here:
+  (proxy({
+    url: '	https://api.twitter.com/oauth/access_token' + request.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.TWITTER_TOKEN
+    }
+  }))(request, response);
+});
+
 // The serve-all has to be below the specific requests or it overrides them
 app.use(express.static('./'));
 
